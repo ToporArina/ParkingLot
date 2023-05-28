88494018af1b446ebfd3afced816bf2fd289d00e
l000ps <toporoleksandr@gmail.com>
2023-05-28 06:54:20 +0300

    Stage 3/5: Expand and park

--- Main.kt
+++ Main.kt
@@ -1,12 +1,38 @@
 package parking
 
+import kotlin.system.exitProcess
+
+data class Car(var num: String, var color: String)
+
+var parking = Array(20) { "" }
+var inp = listOf<String>()
+
 fun main() {
-    val park = readln().split(" ")
-    if (park[0] == "park") {
-        println("${park[2]} car parked in spot 2.")
-    } else if (park[0] == "leave" && park[1] == "1") {
-        println("Spot 1 is free.")
-    } else if (park[0] == "leave" && park[1] == "2") {
-        println("There is no car in spot 2.")
+    while (true) {
+        inp = readln().split(" ")
+        when (inp[0]) {
+            "park" -> park()
+            "leave" -> leave()
+            "exit" -> exitProcess(0)
+        }
     }
+}
+
+fun park() {
+    for (i in 0 until parking.size) {
+        if (parking[i] != "") {
+            continue
+        } else {
+            parking[i] = "${inp[1]} ${inp[2]}"
+            println("${inp[2]} car parked in spot ${i + 1}.")
+            return
+        }
+    }
+    println("Sorry, the parking lot is full.")
+    return
+}
+
+fun leave() {
+    parking[inp[1].toInt() - 1] = ""
+    println("Spot ${inp[1]} is free.")
 }
